a6ccb4c6775beb4c0827cf4c636f6013a74bf881
BATTIG Dylan <dylan.battig@etu.univ-smb.fr>
2023-11-19 14:55:09 +0000

    Laravel mix + Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
+
