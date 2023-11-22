2cded0495163752222c3e877a4babc18332204f3
RUAULT Maxime <maxime.ruault@etu.univ-smb.fr>
2023-11-22 15:16:57 +0000

    ADD Favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     <title>@yield('title')</title>
 </head>
