df1217955a49171b3d08123db01a9b81fed7d7ce
BATTIG Dylan <dylan.battig@etu.univ-smb.fr>
2023-11-25 19:56:24 +0000

    Design and move to auth controller

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -2,73 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LoginUtilisateurRequest;
-use App\Models\Utilisateur;
 use App\Models\Langue;
 use App\Models\Pays;
 
-use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Auth;
 
-use App\Http\Requests\RegisterUtilisateurRequest;
 use App\Http\Requests\UpdateUtilisateurRequest;
-
-use Illuminate\Auth\Events\Registered;
-use PhpParser\Node\Expr\FuncCall;
-use Symfony\Component\Console\Command\DumpCompletionCommand;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
-
 
 class UtilisateurController extends Controller
 {
-    public function register()
-    {
-        return view("auth.register", [ 'langues' => Langue::all() , 'pays' => Pays::all() ]);
-    }
-
-    public function doRegister(RegisterUtilisateurRequest $request)
-    {
-        $validated = $request->validated();
-        $user = Utilisateur::create($validated);
-
-        $validated["password"] = $validated["motpasse"];
-        unset($validated["motpasse"]);
-
-        event(new Registered($user));
-
-        if (Auth::attempt($validated)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended(RouteServiceProvider::HOME);
-        }
-
-        return back();
-    }
-
-    public function login()
-    {
-        return view("auth.login");
-    }
-
-    public function doLogin(LoginUtilisateurRequest $request)
-    {
-        $validated = $request->validated();
-
-        $validated["password"] = $validated["motpasse"];
-        unset($validated["motpasse"]);
-
-        if (Auth::attempt($validated)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended(RouteServiceProvider::HOME);
-        }
-
-        return back()->withErrors([
-            'identifiant' => 'Mauvais identifiant ou mot de passe.',
-        ])->withInput();
-    }
-
     public function update()
     {
         return view('account.update', [
@@ -93,31 +35,5 @@ class UtilisateurController extends Controller
         $user->update($validated);
 
         return redirect()->route('accountUpdate');
-    }
-
-    public function logout()
-    {
-        Auth::logout();
-        return redirect()->route('welcome');
-    }
-
-    public function verify() {
-
-        return view('auth.verify');
-
-    }
-
-    public function doVerify(EmailVerificationRequest $request)
-    {
-        $request->fulfill();
-
-        return redirect(RouteServiceProvider::HOME);
-    }
-
-    public function resendVerify(Request $request)
-    {
-        $request->user()->sendEmailVerificationNotification();
-
-        return back()->with('message', 'Email de vérification renvoyer !');
     }
 }
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,9 +1,25 @@
-<h1>Vérifier votre adresse mail !</h1>
+@extends('layouts.app')
 
-<form action="{{ route('doResendVerifyAccount') }}" method="POST">
-@csrf
-    <input type="submit" value="Renvoyer le mail de vérification">
-    @if (session('message'))
-        <p>{{ session('message') }}</p>
+@section('content')
+
+<div class="flex flex-col items-center justify-center gap-1 text-center p-20">
+    <img width="75" src="{{ asset('assets/mail.svg') }}" class="mb-3"/>
+
+    <h1 class="text-4xl font-bold">Confirmer votre adresse mail</h1>
+    <p class="">Merci de confirmer votre adresse mail pour continuer</p>
+    <p class="text-slate-800 underline">{{ $mail }}</p>
+
+    <form action="{{ route('doResendVerifyAccount') }}" method="post">
+        @csrf
+
+        <button type="submit" class="mt-7 bg-black text-white font-bold px-10 py-2 col-span-6 uppercase">Envoyer une confirmation</button>
+    </form>
+
+    @if(session('send'))
+        <p class="text-sm text-slate-800 mt-5">
+            {{ session('send') }}
+        </p>
     @endif
-</form>
+</div>
+
+@endsection
